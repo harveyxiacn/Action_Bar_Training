3b841437c9521c97f68bca91dbaa227f8ef0dbb1
Harvey Xia <hxia.itgarage@outlook.com>
2015-02-13 08:39:57 -0400

    Add the Actions to the Action Bar

--- app/src/main/java/com/itgaragebusappdev/actionbartraining/MainActivity.java
+++ app/src/main/java/com/itgaragebusappdev/actionbartraining/MainActivity.java
@@ -1,8 +1,9 @@
 package com.itgaragebusappdev.actionbartraining;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -17,9 +18,10 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
